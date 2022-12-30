42559aa92f3ac2913dea252ccf00224933d74273
YeonWoo Bae <sleepisparrow@gmail.com>
2022-12-30 21:40:44 +0900

    init-2

---
+++ app/src/main/java/kr/ac/cnu/computer/sgne/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.cnu.computer.sgne;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
