59a1a52c3dd8d1b686a615b4cb3642d7d84f8cc3
성원준 <wseong06@gmail.com>
2023-01-15 21:08:49 +0900

    WonjunSeong_PackagedProjects

---
+++ app/src/main/java/kr/ac/cnu/computer/sgne/quiz_creator/multipleQuiz.java
@@ -0,0 +1,17 @@
+package kr.ac.cnu.computer.quizcreater;
+
+public class multipleQuiz {
+    String solution;
+    String script1;
+    String script2;
+    String script3;
+    String script4;
+
+    multipleQuiz(){
+        this.solution = "";
+        this.script1 = "";
+        this.script2 = "";
+        this.script3 = "";
+        this.script4 = "";
+    }
+}
