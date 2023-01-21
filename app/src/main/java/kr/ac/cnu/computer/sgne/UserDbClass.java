0c53d33adcccb425d539a0f99a9ae71438a90d7d
Jshman <jonghp1128@naver.com>
2023-01-21 14:32:42 +0900

    user의 정보를 DB와 연결해주는 클래스 생성

---
+++ app/src/main/java/kr/ac/cnu/computer/sgne/UserDbClass.java
@@ -0,0 +1,14 @@
+package kr.ac.cnu.computer.sgne;
+
+import android.util.Log;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class UserDbClass {
+//    private DatabaseReference mDatabase;
+//
+//    mDatabase = FirebaseDatabase.getInstance().getReference();
+}
