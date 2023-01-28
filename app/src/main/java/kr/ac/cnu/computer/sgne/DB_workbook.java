ecf08deffe969006b1c834933eaa1f779c2d5fc7
0314PILOVE <chunhe50@naver.com>
2023-01-28 18:50:26 +0900

    Update DB_workbook.java

--- app/src/main/java/kr/ac/cnu/computer/sgne/DB_workbook.java
+++ app/src/main/java/kr/ac/cnu/computer/sgne/DB_workbook.java
@@ -12,14 +12,14 @@ import kr.ac.cnu.computer.sgne.quiz_list.Quiz;
 public class DB_workbook {
 
     // 파이어베이스 데이터베이스 연동
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    private DatabaseReference databaseReference = database.getReference();
+    private static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private static DatabaseReference databaseReference = database.getReference();
 
     String workbookName;
     int personNum;
     int totalPersonNum;
     int totalProblemNum;
-    ArrayList<DB_quiz> quiz_list = new ArrayList<DB_quiz>();
+//    ArrayList<DB_quiz> quiz_list = new ArrayList<DB_quiz>();
     ArrayList<DB_workbookUser> user_list = new ArrayList<DB_workbookUser>();
 
     public String getWorkbookName() {
@@ -54,13 +54,13 @@ public class DB_workbook {
         this.totalProblemNum = totalProblemNum;
     }
 
-    public ArrayList<DB_quiz> getQuiz_list() {
-        return quiz_list;
-    }
+//    public ArrayList<DB_quiz> getQuiz_list() {
+//        return quiz_list;
+//    }
 
-    public void setQuiz_list(ArrayList<DB_quiz> quiz_list) {
-        this.quiz_list = quiz_list;
-    }
+//    public void setQuiz_list(ArrayList<DB_quiz> quiz_list) {
+//        this.quiz_list = quiz_list;
+//    }
 
     public ArrayList<DB_workbookUser> getUser_list() {
         return user_list;
@@ -70,9 +70,8 @@ public class DB_workbook {
         this.user_list = user_list;
     }
 
-    public void putDB_workbook(DB_workbook wb) {
-        databaseReference.child("WorkBook").child(wb.workbookName).setValue(wb.user_list = null);
-        databaseReference.child("WorkBook").child(wb.workbookName).child("User_list").setValue(wb.getUser_list());
+    static void putDB_workbook(DB_workbook wb) {
+        databaseReference.child("WorkBook").child(wb.workbookName).setValue(wb);
+        databaseReference.child("WorkBook").child(wb.workbookName).child("UserList").setValue(wb.getUser_list());
     }
-
 }
